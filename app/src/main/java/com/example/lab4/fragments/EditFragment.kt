9a7e76f238f5bd56c07bf1b9438af0315cd4e4c1
Taymaz Davoodi <41880839+tazmanianDeviloper@users.noreply.github.com>
2022-12-01 21:28:21 -0500

    lab4_redo

--- app/src/main/java/com/example/lab4/fragments/EditFragment.kt
+++ app/src/main/java/com/example/lab4/fragments/EditFragment.kt
@@ -1,11 +1,10 @@
 package com.example.lab4.fragments
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -15,6 +14,7 @@ import com.example.lab4.datalayer.Project
 import com.example.lab4.datalayer.ProjectDao
 import com.example.lab4.viewmodel.CurProjectViewModel
 import com.example.lab4.viewmodel.ProjectListViewModel
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER

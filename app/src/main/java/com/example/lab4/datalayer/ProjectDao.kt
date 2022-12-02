26473c054322f49c0f4ba875dfa4fb6af81a5712
Taymaz Davoodi <41880839+tazmanianDeviloper@users.noreply.github.com>
2022-12-01 20:57:02 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab4/datalayer/ProjectDao.kt
@@ -0,0 +1,32 @@
+package com.example.lab4.datalayer
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+/*
+This ProjectDao interface defines various CRUD operations
+to access the projects table in the database
+ */
+@Dao
+interface ProjectDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addProject(project: Project)
+
+    @Delete
+    fun delProject(project: Project)
+
+    @Update
+    fun editProject(project: Project)
+
+    @Query("SELECT count(*) From projects")
+    fun count(): LiveData<Int>
+
+    @Query("SELECT * FROM projects")
+    fun getAllProjects(): LiveData<List<Project>>
+
+    @Query("SELECT * FROM projects where id = :projId")
+    fun searchProjectById(projId: Long): LiveData<Project>
+
+    @Query("SELECT * FROM projects WHERE title like :projTitle ")
+    fun searchProjectsByTitle(projTitle:String): LiveData<List<Project>>
+}
+
